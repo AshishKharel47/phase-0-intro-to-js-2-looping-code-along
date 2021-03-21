19225824d10be9cc35dc4cf28231cd26d46159ab
AshishKharel <ashishkharel58@gmail.com>
2021-03-21 14:05:04 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const names = ["Ada","Brendan","Ali"];
+ 
+function writeCards(names){
+    for (let i=0; i< names.length;i++){
+        console.log(`Thank you, ${names[i]}, for the wonderful birthday gift!`);
+        debugger;
+    }
+    return names;
+}
+writeCards(names); 
+
+
+
+    let number=10;
+    while (number>=0){
+        console.log(number--);
+    }
+    
